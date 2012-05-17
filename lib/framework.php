84d0a283dcb9f9c1c8f49da3bb3f84a9a4633ccd
litvinok <litvinok@gmail.com>
2012-05-17 18:48:43 +0300

    Fixed cpu-times mode

--- lib/framework.php
+++ lib/framework.php
@@ -234,11 +234,12 @@ class PHPLame
             }
 
             $usage[1] = getrusage();
-            $systime = floatval( $usage[1]['ru_stime.tv_sec']. '.' .$usage[1]['ru_stime.tv_usec'] )
-                     - floatval( $usage[0]['ru_stime.tv_sec']. '.' .$usage[0]['ru_stime.tv_usec'] );
 
-            $usrtime = floatval( $usage[1]['ru_utime.tv_sec']. '.' .$usage[1]['ru_utime.tv_usec'] )
-                     - floatval( $usage[0]['ru_utime.tv_sec']. '.' .$usage[0]['ru_utime.tv_usec'] );
+            $systime = (( $usage[1]['ru_stime.tv_sec']*1e6 + $usage[1]['ru_stime.tv_usec']) / 1000000 )
+                     - (( $usage[0]['ru_stime.tv_sec']*1e6 + $usage[0]['ru_stime.tv_usec']) / 1000000 );
+
+            $usrtime = (( $usage[1]['ru_utime.tv_sec']*1e6 + $usage[1]['ru_utime.tv_usec']) / 1000000 )
+                     - (( $usage[0]['ru_utime.tv_sec']*1e6 + $usage[0]['ru_utime.tv_usec']) / 1000000 );
 
             unset( $usage );
 
